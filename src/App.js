83f5f73d5fa84e3a01b9f896c8f251650455d8d2
Lawrence Muema Kioko <lorenkioko@gmail.com>
2024-01-18 19:54:07 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Login from './components/user/login';
+import Register from './components/user/register';
+
+function App() {
+  return (
+    <div className="App">
+      <Login />
+      <Register />
+    </div>
+  );
+}
+
+export default App;
