5cb5d4eb3b8cd9cd25a0f007941aa0d79de253b6
Lawrence Muema Kioko <lorenkioko@gmail.com>
2024-01-22 21:49:56 +0300

    Add routes to the components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import Login from './components/user/login';
 import Register from './components/user/register';
 
 function App() {
   return (
-    <div className="App">
-      <Login />
-      <Register />
-    </div>
+    <Router>
+      <div className="App">
+        <Route path="/" component={Login} />
+        <Route path="/register" component={Register} />
+      </div>
+    </Router>
   );
 }
 
